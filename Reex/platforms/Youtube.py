18dd03ad105a033b16999cd7c48ded4f035e50bf
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-05-18 02:50:40 +0700

    anjay

--- Reex/platforms/Youtube.py
+++ Reex/platforms/Youtube.py
@@ -266,74 +266,46 @@ class YouTubeAPI:
             x.download([link])
             return xyz
 
-        def song_video_dl():
-            formats = f"{format_id}+140"
-            fpath = f"downloads/{title}"
-            ydl_optssx = {
-                "format": formats,
-                "outtmpl": fpath,
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-                "prefer_ffmpeg": True,
-                "merge_output_format": "mp4",
-                "cookiefile": cook,
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            x.download([link])
-
-        def song_audio_dl():
-            fpath = f"downloads/{title}.%(ext)s"
-            ydl_optssx = {
-                "format": format_id,
-                "outtmpl": fpath,
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-                "cookiefile": cook,
-                "prefer_ffmpeg": True,
-                "postprocessors": [
-                    {
-                        "key": "FFmpegExtractAudio",
-                        "preferredcodec": "mp3",
-                        "preferredquality": "192",
-                    }
-                ],
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            x.download([link])
-
-        if songvideo:
-            await loop.run_in_executor(None, song_video_dl)
-            fpath = f"downloads/{title}.mp4"
-            return fpath
-        elif songaudio:
-            await loop.run_in_executor(None, song_audio_dl)
-            fpath = f"downloads/{title}.mp3"
-            return fpath
-        elif video:
-            if await is_on_off(1):
-                direct = True
-                downloaded_file = await loop.run_in_executor(None, video_dl)
-            else:
-                proc = await asyncio.create_subprocess_exec(
-                    "yt-dlp",
-                    "-g",
-                    "-f",
-                    "best[height<=?720][width<=?1280]",
-                    f"{link}",
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE,
-                )
-                stdout, stderr = await proc.communicate()
-                if stdout:
-                    downloaded_file = stdout.decode().split("\n")[0]
-                    direct = None
+        try:
+            if songaudio:
+                info = await loop.run_in_executor(None, audio_dl)
+                filename = f"downloads/{info['id']}.mp3"
+                if os.path.isfile(filename):
+                    return filename, True
                 else:
-                    return
-        else:
-            direct = True
-            downloaded_file = await loop.run_in_executor(None, audio_dl)
-        return downloaded_file, direct
+                    return None, False
+
+            elif songvideo:
+                info = await loop.run_in_executor(None, video_dl)
+                ext = info.get("ext", "")
+                filename = f"downloads/{info['id']}.{ext}"
+                if os.path.isfile(filename):
+                    return filename, True
+                else:
+                    return None, False
+            elif video:
+                ydl_optssx = {
+                    "format": "best[height<=?720][width<=?1280]",
+                    "outtmpl": "downloads/%(id)s.%(ext)s",
+                    "quiet": True,
+                    "nocheckcertificate": True,
+                    "geo_bypass": True,
+                    "cookiefile": cook,
+                }
+
+                def generic_video_dl():
+                    with yt_dlp.YoutubeDL(ydl_optssx) as ydl:
+                        return ydl.extract_info(link, download=True)
+
+                info = await loop.run_in_executor(None, generic_video_dl)
+                ext = info.get("ext", "")
+                filename = f"downloads/{info['id']}.{ext}"
+                if os.path.isfile(filename):
+                    return filename, True
+                else:
+                    return None, False
+            else:
+                return None, False
+        except Exception as e:
+            print(f"Error di download: {e}")
+            return None, False
