550bc3ee8f149851ce29ea9bf92b764aac8e0b71
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-05-18 13:30:40 +0700

    A

--- Reex/plugins/__init__.py
+++ Reex/plugins/__init__.py
@@ -1,19 +1,21 @@
 import glob
-from os.path import dirname, isfile
-
+import os
 
 def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    work_dir = os.path.dirname(__file__)
+    # Cari semua file .py di dalam subfolder di plugins
+    mod_paths = glob.glob(os.path.join(work_dir, "*", "*.py"))
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
+    all_modules = []
+    for f in mod_paths:
+        if os.path.isfile(f) and not f.endswith("__init__.py"):
+            # Dapatkan path relatif terhadap work_dir
+            rel_path = os.path.relpath(f, work_dir)
+            # Ganti separator path ke titik (.) untuk import
+            module_name = rel_path.replace(os.path.sep, ".")[:-3]  # hilangkan ".py"
+            all_modules.append(module_name)
 
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
