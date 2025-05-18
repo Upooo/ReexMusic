a4f4229644376ce4a509bddc2e409a3eb433f9f9
upoo <aryasptnanggra@gmail.com>
2025-05-18 12:47:55 +0700

    Update ping.py

--- Reex/plugins/tools/ping.py
+++ Reex/plugins/tools/ping.py
@@ -26,5 +26,5 @@ async def ping_com(client, message: Message, _):
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
     )
-    
+
 
