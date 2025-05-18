3f9e323571cc4d0e4b084923f9983b1adc9b42e9
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-05-18 19:27:54 +0700

    anjay

--- Reex/plugins/tools/queue.py
+++ Reex/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from Reex import app
+from Reex.misc import db
+from Reex.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from Reex.utils.database import get_cmode, is_active_chat, is_music_playing
+from Reex.utils.decorators.language import language, languageCB
+from Reex.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
