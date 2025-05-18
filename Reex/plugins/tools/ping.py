2378658f9ad6c8370be61fe9e27baa278643b90a
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-05-18 13:15:32 +0700

    A

--- Reex/plugins/tools/ping.py
+++ Reex/plugins/tools/ping.py
@@ -11,7 +11,22 @@ from Reex.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
-
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Anony.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
+    
 @app.on_message(filters.command("id") & ~BANNED_USERS)
 async def cek_id(client, message):
     user = message.reply_to_message.from_user if message.reply_to_message else message.from_user
