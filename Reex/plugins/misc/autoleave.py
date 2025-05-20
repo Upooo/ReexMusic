042b5d0c561c59939a3ae6f005e3f01e5602a701
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-05-20 19:43:52 +0700

    .

--- Reex/plugins/misc/autoleave.py
+++ Reex/plugins/misc/autoleave.py
@@ -40,8 +40,7 @@ async def auto_leave():
                 except:
                     pass
 
-if config.AUTO_LEAVING_ASSISTANT:
-    asyncio.create_task(auto_leave())
+asyncio.create_task(auto_leave())
 
 
 async def auto_end():
